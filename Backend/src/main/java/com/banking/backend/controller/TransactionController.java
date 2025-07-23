b1c3fa2748f959790e9215f116bb78954371b6f8
DenisKinyua <kimenjudenis@gmail.com>
2025-07-23 22:38:35 +0300

    Adding Transaction controller

--- Backend/src/main/java/com/banking/backend/controller/TransactionController.java
+++ Backend/src/main/java/com/banking/backend/controller/TransactionController.java
@@ -1,4 +1,24 @@
 package com.banking.backend.controller;
 
+import com.banking.backend.dto.TransactionRequestDTO;
+import com.banking.backend.service.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/transactions")
+@RequiredArgsConstructor
 public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @PostMapping("/transfer")
+    public ResponseEntity<Void> transferFunds(@RequestBody TransactionRequestDTO request){
+        transactionService.transferFunds(request);
+        return ResponseEntity.ok().build();
+    }
 }
