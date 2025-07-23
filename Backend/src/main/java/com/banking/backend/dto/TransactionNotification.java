76e93ce6c813c1efc0ce14a34b36e22e22d7c873
DenisKinyua <kimenjudenis@gmail.com>
2025-07-23 21:04:17 +0300

    Adding DTOs

--- Backend/src/main/java/com/banking/backend/dto/TransactionNotification.java
+++ Backend/src/main/java/com/banking/backend/dto/TransactionNotification.java
@@ -1,4 +1,13 @@
 package com.banking.backend.dto;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class TransactionNotification {
+    private String transactionId;
+    private String userId;
+    private  String message;
+    private LocalDateTime timestamp;
 }
--- Backend/src/main/java/com/banking/backend/dto/TransactionRequestDTO.java
+++ Backend/src/main/java/com/banking/backend/dto/TransactionRequestDTO.java
@@ -1,4 +1,13 @@
 package com.banking.backend.dto;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class TransactionRequestDTO {
+    private String fromUserId;
+    private String toUserId;
+    private BigDecimal amount;
+    private String description;
 }
