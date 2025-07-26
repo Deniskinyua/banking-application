b6fff09d48aad8b3705f86ebeb8bea9cfb5e0900
DenisKinyua <kimenjudenis@gmail.com>
2025-07-26 20:40:50 +0300

    Adding Account repository

--- Backend/src/main/java/com/banking/backend/repository/AccountRepository.java
+++ Backend/src/main/java/com/banking/backend/repository/AccountRepository.java
@@ -9,7 +9,5 @@ import java.util.Optional;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
-    // Best practice: Name query methods clearly. findByUserId is fine.
-    Optional<Account> findByCustomerId(String customerId); // Changed from fromUserId for generality
-    //findByUserId
+    Optional<Account> findByCustomerId(String customerId);
 }
