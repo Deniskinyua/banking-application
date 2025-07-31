710942c31535e74747e456760c4e99374d70b674
DenisKinyua <kimenjudenis@gmail.com>
2025-07-31 22:20:45 +0300

    TransactionController clean up

--- Backend/src/main/java/com/banking/backend/controller/TransactionController.java
+++ Backend/src/main/java/com/banking/backend/controller/TransactionController.java
@@ -10,11 +10,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * REST Controller for managing financial transactions.
@@ -31,6 +30,7 @@ description = "REST APIs for managing funds transfer operations among users with
 public class TransactionController {
 
     private final TransactionService transactionService;
+    private final Logger logger = LoggerFactory.getLogger(TransactionController.class);
 
     /**
      * Constructor for TransactionController
